f0d97e3efeaa1762aef0d7e84e0081bb30163f21
lgaudreau <44848504+lgaudreau@users.noreply.github.com>
2018-12-08 20:10:41 -0500

    Create motion_time.py
    if detect motion, display current date & time on LCD display.  Will be modified for temp & humidity.

---
+++ motion_time.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Assumes a 2 row LCD connected on 27 and a PIR motion detector with the data connected on board pin 7 (gpio4).
+# Will show the current date and time if it detects motion, updating continuously.  If it does not detect motion after 20min, will turn the display off.
+
+import RPi.GPIO as GPIO
+import time
+import I2C_LCD_driver
+
+GPIO.setmode(GPIO.BOARD)
+PIR_PIN = 7
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+mylcd = I2C_LCD_driver.lcd()
+
+print ("PIR Module Test (CTRL+C to exit)")
+time.sleep(2)
+print ("Ready")
+
+next_time = 0
+count = 0
+
+def main():
+    while(1):
+        if GPIO.input(PIR_PIN) == 0:
+           mylcd.backlight(0)
+        else:
+            print ("Motion Detected")
+            start_time = time.clock()
+            check_time = start_time + 1200
+            while time.clock() < check_time:
+                mylcd.lcd_display_string(time.strftime('%I:%M:%S %p'), 1)
+                mylcd.lcd_display_string(time.strftime('%a %b %d, 20%y'), 2)
+             
+main()
