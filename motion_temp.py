26144fef2156e47c4e12af7d7d8b17f5c177cde7
lgaudreau <44848504+lgaudreau@users.noreply.github.com>
2018-12-21 10:48:33 -0500

    Update motion_temp.py

--- motion_temp.py
+++ motion_temp.py
@@ -2,8 +2,8 @@
 
 # Using a Raspberry Pi to detect motion and display the time & date.
 # Assumes a 2 row LCD connected using I2C and a PIR motion detector with the data connected on board pin 7 (gpio4).
-# Will show the current date and time if it detects motion, checking every 20min but the display updats seconds/minutes in real time.  
-# If it does not detect motion after 20min, will turn the display off and check for further motion.  
+# Will show the current date and time if it detects motion, checking every 20min but the display updats seconds/minutes in real time.
+# If it does not detect motion after 20min, will turn the display off and check for further motion.
 
 # OpenWeatherMap API
 # 89152e1d59efc967dab012d7506138d2
@@ -30,6 +30,7 @@ logpath = "/home/pi/Documents/pylog.txt"
 
 with open(logpath,"w") as log:
     log.write("State, Time, Temperature\n" )
+    log.write("OFF,%s,%s\n" %(time.strftime('%I:%M:%S %p'),GetTemp()))
 
 def WriteLog(state, time, temp):
     with open(logpath,"a+") as log:
@@ -42,7 +43,7 @@ def GetTemp():
     temp = w.get_temperature('celsius')['temp']
 
     return int(round(temp))
-    
+
 def main():
     while(1):
         if GPIO.input(PIR_PIN) == 0:
@@ -50,7 +51,7 @@ def main():
            with open(logpath, 'r') as f:
                 lines = f.read().splitlines()
                 last_line = lines[-1]
-                if ("ON" in last_line) or ("State" in last_line):
+                if "ON" in last_line:
                     WriteLog("OFF",time.strftime('%I:%M:%S %p'),GetTemp())
         else:
             start_time = time.time()
@@ -59,12 +60,12 @@ def main():
             with open(logpath, 'r') as f:
                 lines = f.read().splitlines()
                 last_line = lines[-1]
-                if ("OFF" in last_line) or ("State" in last_line):
+                if "OFF" in last_line:
                     WriteLog("ON",time.strftime('%I:%M:%S %p'),curtemp)
             while time.time() < check_time:
                 humidity = str(int(round(sense.humidity)))
                 temperature = str(int(round(sense.temperature)))
                 mylcd.lcd_display_string("Outside %sC" % (str(curtemp)+chr(223)), 1)
                 mylcd.lcd_display_string("%sC, %s HUM" % (temperature + chr(223), humidity), 2)
-                
+
 main()
